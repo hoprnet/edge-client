7a7666c912eed559381c67505269e202d88084ce
Tibor Csóka <csokat@gmail.com>
2025-12-04 15:25:58 +0100

    fix(deps): hopr-lib

---
+++ src/blokli.rs
@@ -0,0 +1,43 @@
+use hopr_chain_connector::{BasicPayloadGenerator, HoprBlockchainConnector, PayloadGenerator, TempDbBackend, blokli_client::{BlokliClient, BlokliClientConfig, BlokliQueryClient}, errors::ConnectorError};
+use hopr_lib::Keypair;
+use url::Url;
+
+pub use hopr_chain_connector as connector;
+pub use hopr_lib::ChainKeypair;
+
+
+pub const DEFAULT_BLOKLI_URL: &str = "https://blokli.stage.hoprnet.link";
+
+pub type HoprBlockchainSafelessConnector<C> = HoprBlockchainConnector<
+    C,
+    TempDbBackend,
+    BasicPayloadGenerator,
+    <BasicPayloadGenerator as PayloadGenerator>::TxRequest,
+>;
+
+pub async fn with_sefeless_blokli_connector<F, T>(chain_key: &ChainKeypair, blokli_provider: Url, f: F) -> anyhow::Result<T> 
+where
+    F: Fn(&HoprBlockchainSafelessConnector<BlokliClient>) -> T
+{
+    let blokli_client = BlokliClient::new(
+    blokli_provider.as_ref().parse()?,
+    BlokliClientConfig {
+        timeout: std::time::Duration::from_secs(5),
+    });
+
+    let info = blokli_client.query_chain_info().await?;
+    let contract_addrs = serde_json::from_str(&info.contract_addresses.0)
+        .map_err(|e| ConnectorError::TypeConversion(format!("contract addresses not a valid JSON: {e}")))?;
+
+    let payload_gen = BasicPayloadGenerator::new(chain_key.public().to_address(), contract_addrs);
+
+    let connector = HoprBlockchainConnector::new(
+        chain_key.clone(),
+        Default::default(),
+        blokli_client,
+        TempDbBackend::new()?,
+        payload_gen,
+    );
+
+    Ok(f(&connector))
+}
--- src/blokli/constants.rs
+++
@@ -1,9 +0,0 @@
-use hopr_chain_connector::reexports::alloy::primitives::{Address, address, hex};
-
-// wxHOPR Token contract address on Gnosis Chain
-pub const WXHOPR_TOKEN_ADDRESS: Address = address!("0xD4fdec44DB9D44B8f2b6d529620f9C0C7066A2c1");
-// Default target suffix to be appended to Channels contract address
-pub const DEFAULT_TARGET_SUFFIX: [u8; 12] = hex!("010103020202020202020202");
-
-pub const DEPLOY_SAFE_MODULE_AND_INCLUDE_NODES_IDENTIFIER: [u8; 32] =
-    hex!("0105b97dcdf19d454ebe36f91ed516c2b90ee79f4a46af96a0138c1f5403c1cc");
