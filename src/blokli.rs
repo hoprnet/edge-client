d1b992bd656b0029e672ddcb883aaaecb0955081
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-01-06 12:50:14 +0100

    spawn subscription ahead of deployment

--- src/blokli.rs
+++ src/blokli.rs
@@ -88,31 +88,28 @@ impl SafelessInteractor {
         inputs: SafeModuleDeploymentInputs,
     ) -> anyhow::Result<SafeModuleDeploymentResult> {
         let me = self.chain_key.public().to_address();
+        let connector = self.connector.clone();
+        let subscription_handle = tokio::spawn(async move {
+            connector.await_safe_deployment(SafeSelector::Owner(me), SAFE_RETRIEVAL_TIMEOUT).await
+        });
 
         let signed_tx = self.create_safe_deployment_payload(inputs).await?;
-
-        // tokio::spawn(|| async move {
-//        let safe = self
-//            .connector
-//            .await_safe_deployment(SafeSelector::Owner(me), SAFE_RETRIEVAL_TIMEOUT)
-//            .await
-//            .map_err(anyhow::Error::from)?;
-
-        let transaction = connector::blokli_client::BlokliTransactionClient::submit_and_confirm_transaction(
+        let transaction = connector::blokli_client::BlokliTransactionClient::submit_transaction(
             self.connector.client(),
             signed_tx.as_ref(),
-            4,
         )
         .await;
 
         tracing::debug!(?transaction, "safe deployment transaction submitted");
 
+        let safe = subscription_handle
+            .await
+            .map_err(|e| anyhow::anyhow!("safe deployment subscription task failed: {e}"))??;
 
-        unimplemented!("Safe deployment retrieval not implemented yet");
-        // Ok(SafeModuleDeploymentResult {
-            // safe_address: safe.address,
-            // module_address: safe.module,
-        // })
+        Ok(SafeModuleDeploymentResult {
+            safe_address: safe.address,
+            module_address: safe.module,
+        })
     }
 
     pub async fn ticket_stats(&self) -> anyhow::Result<TicketStats> {
