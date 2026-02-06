b5f73de78214da1c3ddbb35ed338b23a7e55e509
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2026-02-06 11:34:59 +0100

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -201,15 +201,9 @@ async fn main() -> anyhow::Result<()> {
         "Starting Edgli"
     );
 
-    let edgli = edgli::Edgli::new(
-        cfg,
-        &args.db_dir_path,
-        hopr_keys,
-        args.blokli_url,
-        |s| {
-            info!(?s, "Initialization stage");
-        },
-    )
+    let edgli = edgli::Edgli::new(cfg, &args.db_dir_path, hopr_keys, args.blokli_url, |s| {
+        info!(?s, "Initialization stage");
+    })
     .await?;
 
     let mut signals =
